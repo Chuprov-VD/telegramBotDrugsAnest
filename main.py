3fbfb7f2ed1c81a8b24acce8c9b7a835ca761433
Vladimir <124632941+Chuprov-VD@users.noreply.github.com>
2023-11-04 14:53:16 +0300

    Update main.py
    Function Extension

--- main.py
+++ main.py
@@ -1,64 +1,111 @@
+import aiogram.dispatcher.storage
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
 from aiogram.utils import executor
 import random
 # from aiogram import F
-from aiogram.dispatcher.filters import Text
 
-
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.storage import FSMContext
 
 with open('Token.txt') as file:
     token = file.readline()
+
 bot = Bot(token)
-db = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
-# @db.message_handler(commands=["start"])
+
+# @dp.message_handler(commands=["start"])
 # async def cmd_start(message: types.Message):
-    # await message.answer(text = f"Привет, {message.from_user.first_name}!")
+# await message.answer(text = f"Привет, {message.from_user.first_name}!")
 
-@db.message_handler(commands="start")
+class RandomStates(StatesGroup):
+    wait_num1 = State()
+    wait_fio_num = State()
+    wait_drugs = State()
+
+
+@dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    hello = ["Привет!"]
     buttons = ["Дай пять!", "Дать пинка!"]
-    comandas = ["/random"]
+    comandas = ["Запустить рандом!", "Добавить пациента"]
+    keyboard.add(*hello)
     keyboard.add(*buttons)
     keyboard.add(*comandas)
     await message.answer("Как будешь здороваться?", reply_markup=keyboard)
-@db.message_handler(Text(equals="Дай пять!"))
-async def with_puree(message: types.Message):
+
+
+@dp.message_handler(lambda message: message.text == "Дай пять!")
+async def with_puree(message: Message):
     await message.reply("Отличный выбор! Лови пять!")
 
 
-@db.message_handler(lambda message: message.text == "Дать пинка!")
-async def without_puree(message: types.Message):
+@dp.message_handler(lambda message: message.text == "Дать пинка!")
+async def without_puree(message: Message):
     await message.reply("Сам дурак!")
-@db.message_handler(commands="random")
-async def cmd_random(message: types.Message):
-    keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(types.InlineKeyboardButton(text="Нажми меня", callback_data="random_value"))
-    await message.answer("Нажмите на кнопку, чтобы бот отправил число от 1 до 100", reply_markup=keyboard)
-@db.callback_query_handler(text="random_value")
-async def send_random_value(call: types.CallbackQuery):
-    a = random.randint(1, 23)
-    b = random.randint(1, 23)
-    while a != b:
-        a = random.randint(1, 23)
-        b = random.randint(1, 23)
-        print(a, b)
-        await call.message.answer(f"{a}, {b}")
-    await call.message.answer(f"Выйгравший билет {a}, Поздравляем!")
 
-#@db.message(F.text.lower() == "дать пять")
+
+@dp.message_handler(lambda message: message.text == "Запустить рандом!")
+async def cmd_random(message: Message):
+    await message.answer(text="Введите № последнего билета: ")
+    await RandomStates.wait_num1.set()
+
+
+@dp.message_handler(state=RandomStates.wait_num1)
+async def get_num1(message: Message, state: FSMContext):
+    print(message.text)
+    a = 1
+    b = int(message.text)
+    x = random.randint(a, b)
+    y = random.randint(a, b)
+    await message.answer(text="Счет! ")
+    while x != y:
+        x = random.randint(a, b)
+        y = random.randint(a, b)
+        print(x, y)
+        await message.answer(f"{x}, {y}")
+    await message.answer(f"Выйгравший билет {x}, Поздравляем!")
+    await state.reset_state()
+
+@dp.message_handler(lambda message: message.text == "Добавить пациента")
+async def cmd_patient(message: Message):
+    await message.answer(text="Введите фамилию, и инициалы пациента, и номер истории")
+    await RandomStates.wait_fio_num.set()
+
+@dp.message_handler(state=RandomStates.wait_fio_num)
+async def get_patient_num(message: Message, state: FSMContext):
+    x = len(message.text) * "-"
+    with open('history.txt', "a") as file:
+        file.write(f"{x}\n{message.text}\n")
+    await message.answer(text="Введите наименования и количество наркотиков")
+    await RandomStates.wait_drugs.set()
+
+@dp.message_handler(state=RandomStates.wait_drugs)
+async def get_patient_num(message: Message, state: FSMContext):
+    with open('history.txt', "a") as file:
+        file.write(f"{message.text}\n - {message.from_user.first_name}")
+    await state.reset_state()
+
+#   keyboard = types.InlineKeyboardMarkup()
+#  keyboard.add(types.InlineKeyboardButton(text="Нажми меня", callback_data="random_value"))
+# await message.answer("Нажмите на кнопку, чтобы бот отправил число от 1 до 100", reply_markup=keyboard)
+
+
+# @dp.message(F.text.lower() == "дать пять")
 # async def with_five(message: types.Message):
 #    await message.reply("Отличный выбор, лови пять!")
 
-# @db.message(F.text.lower() == "дать пинка")
+# @dp.message(F.text.lower() == "дать пинка")
 # async def without_pinka(message: types.Message):
 #    await message.reply("Сам получи пинка")
 
-@db.message_handler(text = "Привет")
+@dp.message_handler(text="Привет!")
 async def hello_func(message: Message):
-    await message.answer(text = f"Привет, {message.from_user.first_name}!")
+    await message.answer(text=f"Привет, {message.from_user.first_name}!")
 
-executor.start_polling(dispatcher = db)
 
+executor.start_polling(dispatcher=dp)
