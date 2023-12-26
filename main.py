660067dfb3b88768c1198db105c5aa86f872992d
chuprov-vd <chuprov271@gmail.com>
2023-12-26 03:00:55 +0300

    new features

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ class RandomStates(StatesGroup):
 
 @dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
+    await message.answer(text = f"Привет, {message.from_user.first_name}!")
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     hello = ["Привет!"]
     buttons = ["Дай пять!", "Дать пинка!"]
@@ -54,21 +55,25 @@ async def cmd_random(message: Message):
     await message.answer(text="Введите № последнего билета: ")
     await RandomStates.wait_num1.set()
 
-
 @dp.message_handler(state=RandomStates.wait_num1)
 async def get_num1(message: Message, state: FSMContext):
-    print(message.text)
+    try:
+        message.text = int(message.text)
+    except ValueError:
+        if type(message.text) != int:
+            await message.answer(text="Введен текст, автоматически исправили на случайное число от 1 до 1000: ")
+            message.text = random.randint(1, 1000)
     a = 1
     b = int(message.text)
-    x = random.randint(a, b)
+    x = 4
     y = random.randint(a, b)
     await message.answer(text="Счет! ")
-    while x != y:
-        x = random.randint(a, b)
+    while x > 0:
+        x = x - 1
         y = random.randint(a, b)
         print(x, y)
-        await message.answer(f"{x}, {y}")
-    await message.answer(f"Выйгравший билет {x}, Поздравляем!")
+        await message.answer(f"{x} - {y}")
+    await message.answer(f"Выйгравший билет {y}, Поздравляем!")
     await state.reset_state()
 
 @dp.message_handler(lambda message: message.text == "Добавить пациента")
@@ -88,8 +93,8 @@ async def get_patient_num(message: Message, state: FSMContext):
 async def get_patient_num(message: Message, state: FSMContext):
     with open('history.txt', "a") as file:
         file.write(f"{message.text}\n - {message.from_user.first_name}")
+        await message.answer(text="Спасибо, данные сохранены на сервере")
     await state.reset_state()
-
 #   keyboard = types.InlineKeyboardMarkup()
 #  keyboard.add(types.InlineKeyboardButton(text="Нажми меня", callback_data="random_value"))
 # await message.answer("Нажмите на кнопку, чтобы бот отправил число от 1 до 100", reply_markup=keyboard)
@@ -109,3 +114,4 @@ async def hello_func(message: Message):
 
 
 executor.start_polling(dispatcher=dp)
+
